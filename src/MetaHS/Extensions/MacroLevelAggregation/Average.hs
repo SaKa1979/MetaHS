2586fa8ce6c9754a76fe2d19229cb0f0cacea497
Sander Kamps <sanderkamps79@gmail.com>
2019-01-26 11:44:47 +0100

    Added distribution function to MacroLevelAggregation
    - This function enables the calculation of metric distribution, iow the occurrence frequency of specific metric values.

--- src/MetaHS/Extensions/MacroLevelAggregation/Average.hs
+++ src/MetaHS/Extensions/MacroLevelAggregation/Average.hs
@@ -1,32 +1,39 @@
 module MetaHS.Extensions.MacroLevelAggregation.Average
     (
-    average
+     average
+    ,frequency
     ) where
 
 import qualified MetaHS.DataModel.MetaModel as MetaModel
 import MetaHS.EDSL.MetaModel
 import qualified Data.Set as Set
 
-average :: RelationKey
-           -> MetaModel.MetaModel
-           -> Double
+-- | Calculates the average for the supplied metric Relation Key
+average :: RelationKey          -- ^ The MetaModel Relation Key (E.g., LCOM).
+        -> MetaModel.MetaModel  -- ^ The MetaModel Containing the associated key.
+        -> Double               -- ^ The average
 average key mm = fromIntegral(addition) / fromIntegral(valuesCount)
     where
     addition = foldr (+) 0 values
     valuesCount = length values
     values = getMetricElements key mm
 
+distribution :: RelationKey          -- ^ The MetaModel Relation Key (E.g., LCOM).
+          -> MetaModel.MetaModel  -- ^ The MetaModel Containing the associated key.
+          -> Set.Set (Int, Int)   -- ^ The set containing tuple(s) (MetricValue,Frequency)
+distribution key mm = Set.fromList $ [(x, countValues x xs) | x <- xs]
+    where
+    xs = getMetricElements key mm
+    countValues x = length.filter(==x)
 
-getMetricElements :: RelationKey
-                  -> MetaModel.MetaModel
-                  -> [Int]
-getMetricElements key mm =
-    map getValue $ Set.toList $getRelation key mm
+-- | Extracts the metric values from the Relation associated with supplied key
+getMetricElements :: RelationKey        -- ^ The MetaModel Relation Key (E.g., LCOM).
+                  -> MetaModel.MetaModel-- ^ The MetaModel Containing the associated key.
+                  -> [Int]              -- ^ The list containing all values of supplied key
+getMetricElements key mm = map getValue $ Set.toList $getRelation key mm
 
 
+-- | Extracts the value of the supplied metric Element Pair
 getValue  :: MetaModel.Pair -- ^ The Module Element to analyze.
-          -> Int            -- ^ The metric information for the given (Element) Pair.
-getValue elementPair =
-    MetaModel.intValue $ snd elementPair
-
-
+          -> Int            -- ^ The metric value for the supplied (Element) Pair.
+getValue elementPair = MetaModel.intValue $ snd elementPair
