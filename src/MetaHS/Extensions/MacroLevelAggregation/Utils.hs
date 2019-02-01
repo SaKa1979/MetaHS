2652d4eb16a42293e5b86ee3fcb954db4f307052
Sander Kamps <sanderkamps79@gmail.com>
2019-02-01 12:41:55 +0100

    Avoid insane metric values from the MetaModel
    - To make sure the metric-values are indeed from the correct type of
    Pair

--- src/MetaHS/Extensions/MacroLevelAggregation/Utils.hs
+++ src/MetaHS/Extensions/MacroLevelAggregation/Utils.hs
@@ -9,11 +9,12 @@ import qualified Data.Set as Set
 -- | Extracts the metric values from the Relation associated with supplied key
 getMetricElements :: RelationKey        -- ^ The MetaModel Relation Key (E.g., LCOM).
                   -> MetaModel.MetaModel-- ^ The MetaModel Containing the associated key.
-                  -> [Int]              -- ^ The list containing all values of supplied key
-getMetricElements key mm = map getValue $ Set.toList $getRelation key mm
-
+                  -> [Int]              -- ^ The list containing all (sane) values associated with the supplied key.
+getMetricElements key mm = [x | Just x <- ms]
+  where ms = map getValue $ Set.toList $getRelation key mm
 
 -- | Extracts the value of the supplied metric Element Pair
 getValue  :: MetaModel.Pair -- ^ The Module Element to analyze. Should be (Module,IntValue)
-          -> Int            -- ^ The metric value for the supplied (Element) Pair.
-getValue elementPair = MetaModel.intValue $ snd elementPair
+          -> Maybe Int      -- ^ The metric value for the supplied (Module,IntValue) Pair
+getValue elementPair@(MetaModel.Module _, MetaModel.IntValue _) = Just (MetaModel.intValue $ snd elementPair)
+getValue _ = Nothing
