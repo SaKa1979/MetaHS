6baf3175ef42c9786a320eef1b91972d59a0d432
sander <sanderkamps79@gmail.com>
2019-02-17 14:05:40 +0100

    Switched position of frequency and value in distribution
    Makes more sense as (frequency, value)

--- src/MetaHS/Extensions/MacroLevelAggregation/Distribution.hs
+++ src/MetaHS/Extensions/MacroLevelAggregation/Distribution.hs
@@ -8,8 +8,8 @@ import qualified Data.Set as Set
 
 distribution :: RelationKey          -- ^ The MetaModel Relation Key (E.g., LCOM).
              -> MetaModel.MetaModel  -- ^ The MetaModel Containing the associated key.
-             -> Set.Set (Int, Int)   -- ^ The set containing tuple(s) (MetricValue,Frequency)
-distribution key mm = Set.fromList [(x, countValues x xs) | x <- xs]
+             -> Set.Set (Int, Int)   -- ^ The set containing tuple(s) (Frequency, MetricValue)
+distribution key mm = Set.fromList [(countValues x xs, x) | x <- xs]
     where
     xs = getMetricElements key mm
     countValues x = length.filter(==x)
